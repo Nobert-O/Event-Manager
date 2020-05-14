a71b4fe24535fbfe264b65a6aa7c2b0e9913529d
Nobert-O <54095466+Nobert-O@users.noreply.github.com>
2020-05-14 13:02:06 +0200

    Add files via upload

---
+++ user/simpleusers/config.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+	/**
+	* Configuration file
+	*
+	* This should be the only file you need to edit in, regarding the original script.
+	* Please provide your MySQL login information below.
+	*/
+
+	$GLOBALS["mysql_hostname"] = "localhost";
+	$GLOBALS["mysql_username"] = "root";
+	$GLOBALS["mysql_password"] = "";
+	$GLOBALS["mysql_database"] = "ALUSM";
+
+	// $GLOBALS["mysql_hostname"] = "localhost";
+	// $GLOBALS["mysql_username"] = "nsint_ssplanner";
+	// $GLOBALS["mysql_password"] = "919zhiGTyN";
+	// $GLOBALS["mysql_database"] = "nsint_ssplanner";
+
+?>
